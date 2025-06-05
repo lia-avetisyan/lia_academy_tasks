4f5f0904cc8abe04de013d7045ef90349fc3991f
Lia Avetisyan <avetisyanlia0@gmail.com>
2025-06-05 20:38:50 +0400

    Matrix and String

---
+++ matrix.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "matrix.h"
+
+void MyMatrix::set(size_t row, size_t col, double value) {
+    if (row < rows_ && col < cols_) {
+        data_[row][col] = value;
+    }
+}
+
+double MyMatrix::get(size_t row, size_t col) const {
+    if (row < rows_ && col < cols_) {
+        return data_[row][col];
+    }
+}
+
+size_t MyMatrix::getRows() const {
+    return rows_;
+}
+
+size_t MyMatrix::getCols() const {
+    return cols_;
+}
+void MyMatrix::print() const {
+    for (int i = 0; i < rows_; ++i) {
+        for (int j = 0; j < cols_; ++j) {
+            std::cout << data_[i][j] << " ";
+        }
+        std::cout << '\n';
+    }
+
+}
+
+
+
+int main() {
+
+}
